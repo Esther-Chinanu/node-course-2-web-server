4c1e190620312be47e4625decb2c4cedd8095878
Esther-Chinanu <maduforesther@gmail.com>
2018-07-06 13:39:35 -0700

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -7,23 +7,24 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
-    var now = new Date().toString()
-    var log = `${now}: ${req.method} ${req.url}`
+app.use((req, res, next)  => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
 
     console.log(log);
-    fs.appendFile('server.log', log + '/n');
+    fs.appendFile('server.log', log + '\n');
     next();
 });
 
-// app.use((req, res, next) => {
+// app.use((req, res, next) {
 //     res.render('maintenance.hbs');
 // });
 
-hbs.registerHelper('getCurrentYear', () => {
- return new Date().getFullYear()
+app.use(express.static(__dirname + '/public'));
+
+registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
@@ -31,26 +32,25 @@ hbs.registerHelper('screamIt', (text) => {
 });
 
 app.get('/', (req, res) => {
-   res.render('home.hbs', {
-       pageTitle: 'Home Page',
-       welcomeMessage: 'Welcome to my website',
-   });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my Website'
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about.hbs', {
+    res.render('about.hbs', { 
         pageTitle: 'About Page'
-        
     });
 });
 
 // /bad -send back json with errorMessage
 app.get('/bad', (req, res) => {
     res.send({
-        errorMessage:'Unable to handle request'
-    })
-})
+        errorMessage: 'Unable to handle request'
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
-});
+})
